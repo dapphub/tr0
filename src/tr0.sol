90ec8f4b65e70f5766014bc0d4d1cdc41e20496e
David Terry <me@xwvvvvwx.com>
2021-08-02 09:24:42 +0200

    compiles

--- src/tr0.sol
+++ src/tr0.sol
@@ -1,7 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.6;
+
 import "tr-base/tr-base.sol";
 
 contract V0 {
-    function show() return (string) {
+    function show() external pure returns (string memory) {
         return version();
     }
 }
